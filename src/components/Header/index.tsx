22c51e84685215aedde7b62411c3d89a1752f82e
Sotatek-HuyLe4 <huy.le4@sotatek.com>
2026-07-09 16:31:33 +0700

    feat: add header for layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,34 @@
-import "./index.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./index.module.scss";
 
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <header className={styles.header}>
+      <Link href="/" className={styles.brandSection}>
+        <Image
+          src="/images/logo.png"
+          alt="TSS logo"
+          width={44}
+          height={44}
+          className={styles.logo}
+          priority
+        />
+        <div className={styles.brandText}>
+          <p className={styles.title}>TSS Service</p>
+          <p className={styles.description}>
+            Secure, distributed signing for the TSS project
+          </p>
+        </div>
+      </Link>
+
+      <div className={styles.statusBadge}>
+        <span className={styles.statusDot} />
+        <span className={styles.statusText}>OPERATIONAL</span>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
