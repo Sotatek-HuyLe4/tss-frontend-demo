eb7c5f82815682be038ca7a7f4f96e40c459f8a0
Sotatek-HuyLe4 <huy.le4@sotatek.com>
2026-07-13 14:51:41 +0700

    feat: add env and http request utils

---
+++ src/utils/env.ts
@@ -0,0 +1,8 @@
+export default {
+  public: {
+    url: {
+      tssBackend: process.env.NEXT_PUBLIC_TSS_BACKEND_URL!,
+    },
+  },
+  private: {},
+};
---
+++ src/utils/httpRequest.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosRequestConfig, AxiosDefaults } from "axios";
+
+import env from "./env";
+
+const httpRequestClient = axios.create({
+  baseURL: `${env.public.url.tssBackend}/v1`,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 60_000, // 60 seconds
+});
+
+const httpRequest = async ({
+  ...options
+}: AxiosRequestConfig<AxiosDefaults>) => {
+  try {
+    const response = await httpRequestClient({ ...options });
+
+    return response;
+  } catch (error: any) {
+    console.error(error);
+
+    return error?.response;
+  }
+};
+
+export default httpRequest;
